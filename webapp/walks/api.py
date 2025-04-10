4cacbee9e332fbdaa8ac3cd9e5d4beb56dea35d8
Sean Marcus Jensen <itskolesean@gmail.com>
2025-04-10 16:33:42 +0200

    ignore

--- webapp/walks/api.py
+++ webapp/walks/api.py
@@ -12,10 +12,13 @@ class VideoUploadView(APIView):
 
     def post(self, request, *args, **kwargs):
         serializer = VideoUploadSerializer(data=request.data)
-        if serializer.is_valid():
-            video_file = serializer.validated_data['video']
-            video_file_path = store_video(video_file)
 
-            # TODO: You can save it or pass it to your AI logic here
-            return Response({"message": "Video received.", "url": video_file_path}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        video_file = serializer.validated_data['video']
+        video_file_path = store_video(video_file)
+        # TODO: You can save it or pass it to your AI logic here
+
+        return Response(
+            {"message": "Video received.", "url": video_file_path}, status=status.HTTP_200_OK)
