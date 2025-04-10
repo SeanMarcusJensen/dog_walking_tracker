de28a76d18ff4f6d9a9d9c2be148289b1143061d
Sean Marcus Jensen <itskolesean@gmail.com>
2025-04-10 13:36:40 +0200

    initial

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+
+    # Custom apps
+    'dashboard',
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
