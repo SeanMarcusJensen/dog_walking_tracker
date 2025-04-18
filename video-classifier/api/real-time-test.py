235e3a26fd1b7edbea80db59ec64aa0c082b15a3
Itskolesean <itskolesean@gmail.com>
2025-04-18 16:59:11 +0200

    kk

--- video-classifier/api/real-time-test.py
+++ video-classifier/api/real-time-test.py
@@ -1,5 +1,22 @@
 import cv2
 import mediapipe as mp
+from ultralytics import YOLO
+
+video_path = '/Users/jensen/Exams/dog_walking_tracker/demo_videos/dog_walk_in.mp4'
+
+yolo_model = YOLO('yolo11n.pt')
+
+frame_data = {
+    'x': 120,
+    'y': 0,
+    'width': 500,
+    'height': 750
+}
+
+frame_x1 = frame_data['x']
+frame_y1 = frame_data['y']
+frame_x2 = frame_x1 + frame_data['width']
+frame_y2 = frame_y1 + frame_data['height']
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -60,12 +77,26 @@ def gesture_from_fingers(fingers_up):
         return f"{fingers_up} fingers"
 
 
-cap = cv2.VideoCapture('/home/jensen/Documents/dog_walk_in.mp4')  # Use webcam
+cap = cv2.VideoCapture(video_path)
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
+
+
+     # === YOLO ===
+    results = yolo_model(frame, verbose=False)[0]
+
+    for result in results.boxes:
+        x1, y1, x2, y2 = map(int, result.xyxy[0])
+        conf = result.conf[0]
+        cls_id = int(result.cls[0])
+        label = yolo_model.names[cls_id]
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 100, 0), 2)
+        cv2.putText(frame, f"{label} {conf:.2f}", (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 100, 0), 2)
 
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands_model.process(frame_rgb)
@@ -82,6 +113,11 @@ while cap.isOpened():
     # Display gesture on frame
     cv2.putText(frame, f"Gesture: {gesture_text}", (10, 40),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    overlay = frame.copy()
+    cv2.rectangle(frame, (frame_x1, frame_y1), (frame_x2, frame_y2), (42, 42, 165), 5)
+    cv2.rectangle(frame, (frame_x1, frame_y1), (frame_x2, frame_y2), (42, 42, 165), -1)
+    cv2.addWeighted(overlay, .6, frame, 1 - .6, 0, frame)
 
     cv2.imshow("Hand Gesture Detection", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
