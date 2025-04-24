870d9d095a6813c95ba21d0eccef8409f4b0e6c5
Sean Marcus Jensen <itskolesean@gmail.com>
2025-04-24 13:28:52 +0200

    gg

--- webapp/dashboard/views.py
+++ webapp/dashboard/views.py
@@ -17,14 +17,23 @@ def index(request):
     else:
         last_walk_events = []
 
+    last_poo = last_walk_events.filter(
+        event_type='poo').order_by('-id').first()
+    last_poo_time = last_poo.walk.start_time if last_poo else None
+    time_since_last_poo = timezone.now() - last_poo_time if last_poo else 0
+
     time_since = timezone.now() - last_walk.end_time if last_walk else 0
 
     context = {
         'last_walk': {
             'start_time': last_walk.start_time,
-            'time_since': time_since,
-            'duration': last_walk.get_duration() if last_walk else 0,
+            'time_since': time_since.total_seconds() / 60,  # minutes
+            'duration': last_walk.get_duration().total_seconds() / 60 if last_walk else 0,
         },
+        'events': {
+            'last_poo': last_poo_time,
+            'time_since_last_poo': time_since_last_poo.total_seconds() / 60,  # minutes
+        }
     }
 
     return render(request, 'dashboard/index.html', context=context)
